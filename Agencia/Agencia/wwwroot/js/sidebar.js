557ebb525b94b76c70a76cc67b8d0537ac4094ab
mateo-cecchi-davinci <89209029+mateo-cecchi-davinci@users.noreply.github.com>
2023-11-13 02:35:34 -0300

    feat/home

---
+++ Agencia/Agencia/wwwroot/js/sidebar.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+window.onload = function () {
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#btn");
+    const searchBtn = document.querySelector(".bx-search");
+    const menu = document.querySelector(".bx-menu");
+
+    closeBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+    });
+
+    searchBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+    });
+
+    menu.addEventListener("click", function () {
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+    });
+
+    function menuBtnChange() {
+        if (sidebar.classList.contains("open")) {
+            closeBtn.classList.add("bx", "bx-left-arrow-alt", "pt-3");
+        } else {
+            closeBtn.classList.remove("bx", "bx-left-arrow-alt", "pt-3");
+        }
+    }
+};
